951755b74fe494f4f050351672c5f9692f66553a
Raphael <raphael.hauk@racing.tuwien.ac.at>
2021-06-10 00:17:16 +0200

    fix encoding

--- server/Program.cs
+++ server/Program.cs
@@ -20,24 +20,24 @@ namespace Wettma
             if (!await context.Games.AnyAsync())
             {
                 var mesz = TimeSpan.FromHours(2);
-                await context.Games.AddAsync(new Game() { Id = 1, Time = new DateTimeOffset(2021, 6, 11, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Türkei", Team2 = "Italien" });
+                await context.Games.AddAsync(new Game() { Id = 1, Time = new DateTimeOffset(2021, 6, 11, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "TÃ¼rkei", Team2 = "Italien" });
                 await context.Games.AddAsync(new Game() { Id = 2, Time = new DateTimeOffset(2021, 6, 12, 15, 0, 0, 0, mesz).UtcDateTime, Team1 = "Wales", Team2 = "Schweiz" });
-                await context.Games.AddAsync(new Game() { Id = 13, Time = new DateTimeOffset(2021, 6, 16, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Türkei", Team2 = "Wales" });
+                await context.Games.AddAsync(new Game() { Id = 13, Time = new DateTimeOffset(2021, 6, 16, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "TÃ¼rkei", Team2 = "Wales" });
                 await context.Games.AddAsync(new Game() { Id = 14, Time = new DateTimeOffset(2021, 6, 16, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Italien", Team2 = "Schweiz" });
-                await context.Games.AddAsync(new Game() { Id = 25, Time = new DateTimeOffset(2021, 6, 20, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Schweiz", Team2 = "Türkei" });
+                await context.Games.AddAsync(new Game() { Id = 25, Time = new DateTimeOffset(2021, 6, 20, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Schweiz", Team2 = "TÃ¼rkei" });
                 await context.Games.AddAsync(new Game() { Id = 26, Time = new DateTimeOffset(2021, 6, 20, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Italien", Team2 = "Wales" });
-                await context.Games.AddAsync(new Game() { Id = 3, Time = new DateTimeOffset(2021, 6, 12, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Dänemark", Team2 = "Finnland" });
+                await context.Games.AddAsync(new Game() { Id = 3, Time = new DateTimeOffset(2021, 6, 12, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "DÃ¤nemark", Team2 = "Finnland" });
                 await context.Games.AddAsync(new Game() { Id = 4, Time = new DateTimeOffset(2021, 6, 12, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Belgien", Team2 = "Russland" });
                 await context.Games.AddAsync(new Game() { Id = 15, Time = new DateTimeOffset(2021, 6, 16, 15, 0, 0, 0, mesz).UtcDateTime, Team1 = "Finnland", Team2 = "Russland" });
-                await context.Games.AddAsync(new Game() { Id = 16, Time = new DateTimeOffset(2021, 6, 17, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Dänemark", Team2 = "Belgien" });
-                await context.Games.AddAsync(new Game() { Id = 27, Time = new DateTimeOffset(2021, 6, 21, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Russland", Team2 = "Dänemark" });
+                await context.Games.AddAsync(new Game() { Id = 16, Time = new DateTimeOffset(2021, 6, 17, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "DÃ¤nemark", Team2 = "Belgien" });
+                await context.Games.AddAsync(new Game() { Id = 27, Time = new DateTimeOffset(2021, 6, 21, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Russland", Team2 = "DÃ¤nemark" });
                 await context.Games.AddAsync(new Game() { Id = 28, Time = new DateTimeOffset(2021, 6, 21, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Finnland", Team2 = "Belgien" });
-                await context.Games.AddAsync(new Game() { Id = 6, Time = new DateTimeOffset(2021, 6, 13, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Österreich", Team2 = "Nordmazedonien" });
+                await context.Games.AddAsync(new Game() { Id = 6, Time = new DateTimeOffset(2021, 6, 13, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Ã–sterreich", Team2 = "Nordmazedonien" });
                 await context.Games.AddAsync(new Game() { Id = 5, Time = new DateTimeOffset(2021, 6, 13, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Niederlande", Team2 = "Ukraine" });
                 await context.Games.AddAsync(new Game() { Id = 18, Time = new DateTimeOffset(2021, 6, 17, 15, 0, 0, 0, mesz).UtcDateTime, Team1 = "Ukraine", Team2 = "Nordmazedonien" });
-                await context.Games.AddAsync(new Game() { Id = 17, Time = new DateTimeOffset(2021, 6, 17, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Niederlande", Team2 = "Österreich" });
+                await context.Games.AddAsync(new Game() { Id = 17, Time = new DateTimeOffset(2021, 6, 17, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Niederlande", Team2 = "Ã–sterreich" });
                 await context.Games.AddAsync(new Game() { Id = 29, Time = new DateTimeOffset(2021, 6, 21, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Nordmazedonien", Team2 = "Niederlande" });
-                await context.Games.AddAsync(new Game() { Id = 30, Time = new DateTimeOffset(2021, 6, 21, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Ukraine", Team2 = "Österreich" });
+                await context.Games.AddAsync(new Game() { Id = 30, Time = new DateTimeOffset(2021, 6, 21, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Ukraine", Team2 = "Ã–sterreich" });
                 await context.Games.AddAsync(new Game() { Id = 7, Time = new DateTimeOffset(2021, 6, 13, 15, 0, 0, 0, mesz).UtcDateTime, Team1 = "England", Team2 = "Kroatien" });
                 await context.Games.AddAsync(new Game() { Id = 8, Time = new DateTimeOffset(2021, 6, 14, 15, 0, 0, 0, mesz).UtcDateTime, Team1 = "Schottland", Team2 = "Tschechien" });
                 await context.Games.AddAsync(new Game() { Id = 19, Time = new DateTimeOffset(2021, 6, 18, 18, 0, 0, 0, mesz).UtcDateTime, Team1 = "Kroatien", Team2 = "Tschechien" });
