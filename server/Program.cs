155d9133f04738f13471436359ef2b838bad6cc1
Raphael <raphael.hauk@racing.tuwien.ac.at>
2021-07-08 00:31:33 +0200

    Finale

--- server/Program.cs
+++ server/Program.cs
@@ -72,7 +72,8 @@ namespace Wettma
                 new Game() { Id = 48, Time = new DateTimeOffset(2021, 7,  3, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Ukraine", Team2 = "England", Points = 5.0, DoesNotSupportDraw = true },
 
                 new Game() { Id = 49, Time = new DateTimeOffset(2021, 7,  6, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Italien", Team2 = "Spanien", Points = 6.0, DoesNotSupportDraw = true },
-                new Game() { Id = 50, Time = new DateTimeOffset(2021, 7,  7, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "England", Team2 = "Dänemark", Points = 6.0, DoesNotSupportDraw = true }
+                new Game() { Id = 50, Time = new DateTimeOffset(2021, 7,  7, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "England", Team2 = "Dänemark", Points = 6.0, DoesNotSupportDraw = true },
+                new Game() { Id = 51, Time = new DateTimeOffset(2021, 7,  11, 21, 0, 0, 0, mesz).UtcDateTime, Team1 = "Italien", Team2 = "England", Points = 8.0, DoesNotSupportDraw = true }
             };
             foreach (var game in games)
             {
